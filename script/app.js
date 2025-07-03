691e92388db034317413b24707504c0a7449bbde
G-iTech <G-iTech@hotmail.com>
2025-07-03 06:48:36 +0530

    Initial structure for Volunteer Yatra website

---
+++ script/app.js
@@ -0,0 +1,9 @@
+// Load Header
+fetch('components/header.html')
+  .then(res => res.text())
+  .then(data => document.getElementById('header').innerHTML = data);
+
+// Load Footer
+fetch('components/footer.html')
+  .then(res => res.text())
+  .then(data => document.getElementById('footer').innerHTML = data);
